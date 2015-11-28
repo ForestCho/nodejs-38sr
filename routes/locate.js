303059d39d58e93e5c0e4e9687e9632f390d6bd7
ForestCho <caosl158@gmail.com>
2015-11-28 16:54:46 +0800

    增加新链接处理

---
+++ routes/locate.js
@@ -0,0 +1,91 @@
+var UserDao = require('../dao/userdao');
+var ZymryjDao = require('../dao/zymryjdao');
+var util = require('../lib/util');
+var ArticleDao = require('../dao/articledao');
+var moment = require('moment');
+var config = require('../config').config;
+var cache = require('../common/cache');
+var EventProxy = require('eventproxy');
+var mongoose = require('mongoose');
+/*
+ * 公共查询函数
+ */
+var commonQuery = function(req, res, curpath, articleLimit, cataZh, locatename) {
+    var p = 1;//pageid
+    var pagesize = config.index.list_article_size;
+    var puretext = true;
+    var list_hot_user_size = config.index.list_hot_user_size;
+    var count = 0;
+    if (req.query.p) {
+        p = req.query.p;
+    }
+    if (req.session.user) {
+        res.locals.userinfo = req.session.user;
+    }
+    var ep = new EventProxy();
+    ep.assign("count", "articlelist", function(count, articlelist) {
+        var d = [];
+        d.count = count;
+        d.locatename = locatename;
+        d.data = articlelist;
+        console.log(articlelist.length);
+        res.render('locate', {
+            title: '做一名简单的锶者!',
+            curpath: curpath,
+            d: d,
+            p: p,
+            cataZh: cataZh
+        });
+    });
+
+    ArticleDao.getNumberOfArticlesAsObect(articleLimit, function(err, count) {
+        ep.emit("count", count);
+    });
+
+    ArticleDao.getArticleListLimitAsObject(puretext, p, pagesize, articleLimit, function(err, articlelist) {
+        for (var i = 0; i < articlelist.length; i++) {
+            var b = /<img[^>]+src="[^"]+"[^>]*>/g;
+            var imglist = articlelist[i].content.match(b)
+            var newcontent = articlelist[i].purecontent;
+            if (articlelist[i].classify == 1) {
+                articlelist[i].title = encodeURIComponent(articlelist[i].title);
+            }
+            if (imglist !== null) {
+                if (imglist.length > 0) {
+                    var srcReg = /http:\/\/([^"]+)/i;
+                    var srcStr = imglist[0].match(srcReg);
+                    if (articlelist[i].type == 1) {
+                        var imgWrap = "<a rel='fancypic' href='" + srcStr[0] + "'><img src='" + srcStr[0].replace('large', 'wap180') + "' class='thumb'></a>"
+                    } else {
+                        var imgWrap = "<a rel='fancypic' href='" + srcStr[0] + "'><img src='" + srcStr[0] + "!limitmax" + "' class='thumb'></a>"
+                    }
+                    newcontent = imgWrap + newcontent.substring(0, (newcontent.length > 150) ? 150 : newcontent.length).trim();
+                }
+            } else {
+                newcontent = newcontent.substring(0, (newcontent.length > 180) ? 180 : newcontent.length).trim();
+            }
+            articlelist[i].newcontent = newcontent;
+            console.log(articlelist.length);
+        }
+
+        ep.emit("articlelist", articlelist);
+    })
+
+}
+
+/*
+ * 标签页面
+ */
+exports.index = function(req, res) {
+    var locatename = req.params.locatename;
+    var flag = 0; 
+    var cataZh = "篇文章";
+    var curpath = "/article";
+    res.locals.pageflag = 5;  
+    var articleLimit = { 
+        flag: flag,
+        isdelete: false,
+        location: locatename
+    };
+    commonQuery(req, res, curpath, articleLimit, cataZh, locatename);
+}
